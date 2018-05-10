56da48f97c35e2c89048b52ab31a68047b278b3a
slledru <slledru@msn.com>
2018-05-10 09:23:15 -0600

    async/await

--- Fetch/main.js
+++ Fetch/main.js
@@ -1,14 +1,15 @@
 var fetch = require('node-fetch');
 
-function plainFetch() {
+async function plainFetch() {
   // this fetch grabs the boulder sub reddit
-  fetch('https://www.reddit.com/r/boulder.json').then(function(response) {
-    // the json method on the response object also returns a promise
-    return response.json();
-  }).then(function(json) {
-    // so any additional json processing must happen in another then
-    console.log(json.data.children[0]);
-  });
+  const response = await fetch('https://www.reddit.com/r/boulder.json')
+  if (response.status === 200) {
+    const json = await response.json()
+    console.log(json.data.children[0])
+  }
+  else {
+    console.log('error response', response)
+  }
 }
 
 plainFetch();
